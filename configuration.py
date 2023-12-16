b10a0dd48d8e92937579591e93b7eb920bced1dc
vartem99 <vergolasov57@gmail.com>
2023-12-16 15:34:10 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,5 @@
+URL_SERVICE = 'https://f62dcafa-c3a8-42f2-8b0c-2ff910b4c1bd.serverhub.praktikum-services.ru'
+DOC_PATH = '/docs/'
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
+
---
+++ sender_stand_request.py
@@ -0,0 +1,14 @@
+import configuration
+import requests
+import data
+def post_products_kits(products_ids):
+    return requests.post(configuration.URL_SERVICE + configuration.PRODUCTS_KITS_PATH, json=products_ids,
+                         headers=data.headers)
+
+
+
+
+
+
+
+
